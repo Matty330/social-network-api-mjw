1874494bbac0f79886d993f8fd84e7e880999170
Matt <mwelc64@yahoo.com>
2025-02-10 22:09:26 -0500

    Added professional comments to userController.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 // server.js
 
 const express = require('express');
-const mongoose = require('mongoose');
-require('./config/connection');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-// Import your routes here
-// const routes = require('./routes');
-// app.use(routes);
-
-mongoose.connection.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
